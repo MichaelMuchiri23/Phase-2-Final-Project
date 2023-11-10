33bc63c2bde0349186b2cc475067a39f412db211
Michael Muchiri <muchirimichael2222@gmail.com>
2023-11-10 12:47:23 +0300

    Created Search Engine Component

---
+++ anime-search-engine/src/SearchEngine.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "./Logo";
+import Search from "./Search";
+import Title from "./Title";
+
+function SearchEngine(){
+    return(
+        <>
+         <Title />
+         <Logo />
+         <Search /> 
+        </>
+    )
+}
+
+export default SearchEngine
