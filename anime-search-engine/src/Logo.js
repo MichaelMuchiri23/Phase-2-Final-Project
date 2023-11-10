82f393d2ffeb6c895bb5a4c891dbddcc991f7cc1
Michael Muchiri <muchirimichael2222@gmail.com>
2023-11-10 12:01:05 +0300

    Created Logo component

---
+++ anime-search-engine/src/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Logo(){
+    return(
+        <img id="logo" 
+             src="https://i.pinimg.com/originals/fd/ae/0f/fdae0fe677b0e77144425f12ac109aff.gif"
+             alt="Website logo"
+        />
+    )
+}
+
+export default Logo
