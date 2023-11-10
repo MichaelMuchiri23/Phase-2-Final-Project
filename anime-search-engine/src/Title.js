c25e085e714eb73bace9cfcea6b7873d68446d00
Michael Muchiri <muchirimichael2222@gmail.com>
2023-11-10 11:47:54 +0300

    Created Title Component

---
+++ anime-search-engine/src/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title(){
+    return(
+        <div id="firstdiv">
+            <h1 id="main-header">ANIME SEARCH ENGINE</h1>
+        </div>
+    )
+}
+
+export default Title
