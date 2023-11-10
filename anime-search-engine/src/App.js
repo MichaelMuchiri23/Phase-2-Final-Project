43da558b89d26ae59c0fad2a226c838963077d6a
Michael Muchiri <muchirimichael2222@gmail.com>
2023-11-10 14:44:52 +0300

    Set up App Component

--- anime-search-engine/src/App.js
+++ anime-search-engine/src/App.js
@@ -1,11 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SearchEngine from "./SearchEngine";
+import Navbar from "./Navbar";
 
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+function App(){
+    return(
+        <div>
+         <Navbar />
+         <Routes>
+            <Route path="/" element={<SearchEngine />} />
+         </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
